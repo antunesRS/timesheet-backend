9492767dc52bda759f96795092ef111ce47c0848
raphaelantunes <antunesrd@gmail.com>
2019-05-19 22:52:18 -0300

    adicionada rota default

--- src/config/server.js
+++ src/config/server.js
@@ -1,8 +1,12 @@
-const port = 3000
+const port = (process.env.PORT || 3000)
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const allowCors = require('./cors')
+
+server.get('/', (req,res) => {
+    res.send('Server is running')
+})
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
